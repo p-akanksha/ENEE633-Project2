d623b0aba1ef4135c40d18c472cbd52091c437bb
Akanksha Patel <p.akanksha94@gmail.com>
2020-12-18 14:45:45 -0500

    corrections in svm

--- svm.py
+++ svm.py
@@ -1,13 +1,15 @@
 # import torch
 import argparse
 # import torchvision
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 from keras.datasets import mnist
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score,confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 ## Hyperparameters - need to move? 
 n_epochs = 10
 batch_size_train = 60000
@@ -17,8 +19,8 @@ momentum = 0.5
 log_interval = 10
 
 random_seed = 1
-torch.backends.cudnn.enabled = False
-torch.manual_seed(random_seed)
+# torch.backends.cudnn.enabled = False
+# torch.manual_seed(random_seed)
 
 def load_data():
   # Load data
@@ -75,12 +77,13 @@ if __name__ == '__main__':
 
     train_data = train_data.reshape((60000,784))
     test_data = test_data.reshape((10000,784))
-    train_label = train_label.reshape((60000,1))
-    test_label = test_label.reshape((10000,1))
+    # train_label = train_label.reshape((60000,))
+    # test_label = test_label.reshape((10000,))
 
     print(train_data.shape)
     print(test_data.shape)
-
+    train_data = train_data/255.0
+    test_data = test_data/255.0 
     # train_data = scale(train_data)
     # test_data = scale(test_data)
     sc = StandardScaler()
@@ -90,7 +93,7 @@ if __name__ == '__main__':
     # Dimensionality Reduction
     if red == 'PCA':
       print("Applying PCA")
-      n_components = 100 
+      n_components = 5 
       pca = PCA(n_components=n_components, svd_solver='randomized',
                 whiten=True)
       train_data = pca.fit_transform(train_data)
@@ -106,20 +109,29 @@ if __name__ == '__main__':
     # Linear Kernel
     print("Training Linear Classifier...")
     clf = SVC(kernel='linear', cache_size=2000)
-    clf.fit(train_data, train_label)
+    clf.fit(train_data[:1000, :], train_label[:1000])
     print("Training complete!")
 
-    pred_labels = classifier.predict(train_data)
+    pred_labels = clf.predict(test_data)
 
-    model_acc = classifier.score(X_test, y_test)
-    test_acc = accuracy_score(y_test, y_pred)
-    conf_mat = confusion_matrix(y_test,y_pred)
+    model_acc = clf.score(test_data, test_label)
+    test_acc = accuracy_score(test_label, pred_labels)
+    conf_mat = confusion_matrix(test_label,pred_labels)
 
     print("Model Performance:")
     print("Model accuracy: ", model_acc)
     print("Test accuracy: ", model_acc)
     print("Confusion matrix:")
     print(model_acc)
+
+    fig = plt.figure(figsize=(10, 10)) # Set Figure
+    # Plot Confusion matrix
+    sns.heatmap(conf_mat.T, square=True, annot=True, cbar=False, cmap=plt.cm.Greens)
+    plt.xlabel('Predicted Values')
+    plt.ylabel('True Values');
+    # plt.show();
+    plt.savefig('linear.png')
+    plt.close()
 
 
     # Polynomial Kernel
@@ -128,17 +140,26 @@ if __name__ == '__main__':
     clf.fit(train_data, train_label)
     print("Training complete!")
 
-    pred_labels = classifier.predict(train_data)
+    pred_labels = clf.predict(test_data)
 
-    model_acc = classifier.score(X_test, y_test)
-    test_acc = accuracy_score(y_test, y_pred)
-    conf_mat = confusion_matrix(y_test,y_pred)
+    model_acc = clf.score(test_data, test_label)
+    test_acc = accuracy_score(test_label, pred_labels)
+    conf_mat = confusion_matrix(test_label,pred_labels)
 
     print("Model Performance:")
     print("Model accuracy: ", model_acc)
     print("Test accuracy: ", model_acc)
     print("Confusion matrix:")
     print(model_acc)
+
+    fig = plt.figure(figsize=(10, 10)) # Set Figure
+    # Plot Confusion matrix
+    sns.heatmap(conf_mat.T, square=True, annot=True, cbar=False, cmap=plt.cm.Greens)
+    plt.xlabel('Predicted Values')
+    plt.ylabel('True Values');
+    # plt.show();
+    plt.savefig(f'poly.png')
+    plt.close()
 
     # RBF Kernel
     print("Training RBF Classifier...")
@@ -146,14 +167,23 @@ if __name__ == '__main__':
     clf.fit(train_data, train_label)
     print("Training complete!")
 
-    pred_labels = classifier.predict(train_data)
+    pred_labels = clf.predict(test_data)
 
-    model_acc = classifier.score(X_test, y_test)
-    test_acc = accuracy_score(y_test, y_pred)
-    conf_mat = confusion_matrix(y_test,y_pred)
+    model_acc = clf.score(test_data, test_label)
+    test_acc = accuracy_score(test_label, pred_labels)
+    conf_mat = confusion_matrix(test_label,pred_labels)
 
     print("Model Performance:")
     print("Model accuracy: ", model_acc)
     print("Test accuracy: ", model_acc)
     print("Confusion matrix:")
     print(model_acc)
+
+    fig = plt.figure(figsize=(10, 10)) # Set Figure
+    # Plot Confusion matrix
+    sns.heatmap(conf_mat.T, square=True, annot=True, cbar=False, cmap=plt.cm.Greens)
+    plt.xlabel('Predicted Values')
+    plt.ylabel('True Values');
+    # plt.show();
+    plt.savefig(f'rbf.png')
+    plt.close()
